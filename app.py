7dfd76465cc0cce191392fb81706eabd2bef742e
Daniel Vila <dvilasuero@users.noreply.huggingface.co>
2024-09-10 13:33:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,26 @@ import gradio as gr
 from src.distilabel_dataset_generator.faq import app as faq_app
 from src.distilabel_dataset_generator.sft import app as sft_app
 
+
+theme = gr.themes.Monochrome(
+    text_size="lg",
+    spacing_size="md",
+    font=[gr.themes.GoogleFont('Inter'), 'ui-sans-serif', 'system-ui', 'sans-serif'],
+    
+).set(
+    input_text_size='*text_md',
+    button_large_text_size='*text_md'
+)
+
+
 demo = gr.TabbedInterface(
     [sft_app, faq_app],
     ["Supervised Fine-Tuning", "FAQ"],
-    title="⚗️ Distilabel Dataset Generator",
-    head="⚗️ Distilabel Dataset Generator",
-    theme="ParityError/Interstellar",
+    title="⚗️ distilabel Dataset Generator",
+    head="⚗️ distilabel Dataset Generator",
+    theme=theme,
 )
+
 
 if __name__ == "__main__":
     demo.launch()
