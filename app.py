cbf899b54dd0deb806f75a98cd92590c361c77b0
Daniel Vila <dvilasuero@users.noreply.huggingface.co>
2024-09-10 15:40:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,10 @@ from src.distilabel_dataset_generator.sft import app as sft_app
 
 
 theme = gr.themes.Monochrome(
-    text_size="lg",
     spacing_size="md",
     font=[gr.themes.GoogleFont('Inter'), 'ui-sans-serif', 'system-ui', 'sans-serif'],
     
-).set(
-    input_text_size='*text_md',
-    button_large_text_size='*text_md'
 )
-
 
 demo = gr.TabbedInterface(
     [sft_app, faq_app],
