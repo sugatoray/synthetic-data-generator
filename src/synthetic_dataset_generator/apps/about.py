af420e8afcff4cac9fa3e7e32a852135200069e4
davidberenstein1957 <david.m.berenstein@gmail.com>
2024-12-12 13:23:17 +0100

    add refrences to distilabel and inference endpoints

--- src/synthetic_dataset_generator/apps/about.py
+++ src/synthetic_dataset_generator/apps/about.py
@@ -7,8 +7,7 @@ with gr.Blocks() as app:
 
         Introducing the Synthetic Data Generator, a user-friendly application that takes a no-code approach to creating custom datasets with Large Language Models (LLMs). The best part: A simple step-by-step process, making dataset creation a non-technical breeze, allowing anyone to create datasets and models in minutes and without any code.
 
-        The synthetic data generator takes your custom prompt and returns a dataset for your use case, using a synthetic data pipeline. In the background this is powered by distilabel and the free Hugging Face text-generation API but we don’t need to worry about these complexities and we can focus on using the UI.
-
+        The synthetic data generator takes your custom prompt and returns a dataset for your use case, using a synthetic data pipeline. In the background this is powered by [distilabel](https://distilabel.argilla.io/latest/) and the [free Hugging Face text-generation API](https://huggingface.co/docs/api-inference/en/index) but we don’t need to worry about these complexities and we can focus on using the UI.
 
         - Read more in [our announcement blog post](https://huggingface.co/blog/synthetic-data-generator)
         - Find the library on [GitHub](https://github.com/argilla-io/synthetic-data-generator)
