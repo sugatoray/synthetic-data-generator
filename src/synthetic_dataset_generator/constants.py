62bb2f63fb56402597ff4621a5fd6ae867c09b20
davidberenstein1957 <david.m.berenstein@gmail.com>
2024-12-17 07:15:31 +0100

    update messaging magpie requirements

--- src/synthetic_dataset_generator/constants.py
+++ src/synthetic_dataset_generator/constants.py
@@ -33,18 +33,21 @@ if BASE_URL != "https://api-inference.huggingface.co/v1/" and len(API_KEYS) == 0
     raise ValueError(
         "API_KEY is not set. Ensure you have set the API_KEY environment variable that has access to the Hugging Face Inference Endpoints."
     )
-if "Qwen2" not in MODEL and "Llama-3" not in MODEL:
+
+llama_options = ["llama3", "llama-3", "llama 3"]
+qwen_options = ["qwen2", "qwen-2", "qwen 2"]
+if MODEL.lower() in llama_options:
+    SFT_AVAILABLE = True
+    MAGPIE_PRE_QUERY_TEMPLATE = "llama3"
+elif MODEL.lower() in qwen_options:
+    SFT_AVAILABLE = True
+    MAGPIE_PRE_QUERY_TEMPLATE = "qwen2"
+else:
     SFT_AVAILABLE = False
     warnings.warn(
-        "SFT_AVAILABLE is set to False because the model is not a Qwen or Llama model."
+        "`SFT_AVAILABLE` is set to `False` because the model is not a Qwen or Llama model."
     )
     MAGPIE_PRE_QUERY_TEMPLATE = None
-else:
-    SFT_AVAILABLE = True
-    if "Qwen2" in MODEL:
-        MAGPIE_PRE_QUERY_TEMPLATE = "qwen2"
-    else:
-        MAGPIE_PRE_QUERY_TEMPLATE = "llama3"
 
 # Embeddings
 STATIC_EMBEDDING_MODEL = "minishlab/potion-base-8M"
